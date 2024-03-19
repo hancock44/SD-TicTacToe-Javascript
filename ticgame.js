70688883a627c3e074534f2d94525045c7e35ba1
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-03-19 13:51:24 -0400

    Update ticgame.js

--- ticgame.js
+++ ticgame.js
@@ -1,14 +1,14 @@
 var currentPlayer = 1; // Player 1 starts
-var gameEnded = false;
+var done = false;
 
-var winConditions = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Horizontal
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Vertical
-    [0, 4, 8], [2, 4, 6]              // Diagonal
+var wins = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+    [0, 4, 8], [2, 4, 6]              
 ];
 
 function changeColor(index) {
-    if (gameEnded) return;
+    if (done) return;
 
     var buttons = document.getElementsByClassName('button');
     var color = currentPlayer === 1 ? 'red' : 'blue';
@@ -17,19 +17,17 @@ function changeColor(index) {
     if (checkWin(color)) {
         document.getElementById('winMessage').innerText = "Player " + currentPlayer + " wins!";
         document.getElementById('winMessage').style.display = "block";
-        gameEnded = true;
+        done = true;
         return;
     }
-    
-    // Switch player
     currentPlayer = currentPlayer === 1 ? 2 : 1;
 }
 
 function checkWin(color) {
-    for (var i = 0; i < winConditions.length; i++) {
+    for (var i = 0; i < wins.length; i++) {
         var count = 0;
-        for (var j = 0; j < winConditions[i].length; j++) {
-            var index = winConditions[i][j];
+        for (var j = 0; j < wins[i].length; j++) {
+            var index = wins[i][j];
             var button = document.getElementsByClassName('button')[index];
             if (button.style.backgroundColor === color) {
                 count++;
@@ -52,6 +50,7 @@ function reset() {
         buttons[i].style.backgroundColor = '#ccc';
     }
     document.getElementById('winMessage').style.display = "none";
+}
     currentPlayer = 1;
-    gameEnded = false;
+    done = false;
 }
