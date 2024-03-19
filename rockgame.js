207e9702547e92d7bd34d73b0c1f7d668f94e342
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-03-19 15:49:36 -0400

    Update rockgame.js

--- rockgame.js
+++ rockgame.js
@@ -1,30 +1,30 @@
-function checkWinner(index) {
+function checkWinner(playerValue) {
   var computerValue = computerShoot();
   
-  if (computerValue == playerValue) {
+  if (computerValue === playerValue) {
     document.getElementById('winMessage').innerText = "A tie!";
-  } else if (computerValue == 1 && playerValue == 2) {
+  } else if (computerValue === 1 && playerValue === 2) {
     document.getElementById('winMessage').innerText = "Player wins!";
-  } else if (computerValue == 2 && playerValue == 1) {
+  } else if (computerValue === 2 && playerValue === 1) {
     document.getElementById('winMessage').innerText = "Computer wins!";
-  } else if (computerValue == 3 && playerValue == 2) {
+  } else if (computerValue === 3 && playerValue === 2) {
     document.getElementById('winMessage').innerText = "Computer wins!";
-  } else if (computerValue == 2 && playerValue == 3) {
+  } else if (computerValue === 2 && playerValue === 3) {
     document.getElementById('winMessage').innerText = "Player wins!";
-  } else if (computerValue == 1 && playerValue == 3) {
+  } else if (computerValue === 1 && playerValue === 3) {
     document.getElementById('winMessage').innerText = "Computer wins!";
-  } else if (computerValue == 3 && playerValue == 1) {
+  } else if (computerValue === 3 && playerValue === 1) {
     document.getElementById('winMessage').innerText = "Player wins!";
   }
 }
 
 function computerShoot() {
-  var value = getRandomInt(1,4);
+  var value = getRandomInt(1, 4);
   return value;
 }
 
 function getRandomInt(min, max) {
   const minCeiled = Math.ceil(min);
   const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1)) + minCeiled;
 }
