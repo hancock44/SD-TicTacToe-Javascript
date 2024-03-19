fc9d44410788d629360a5dbb63bea214fadb1db1
hancock44 <157066207+hancock44@users.noreply.github.com>
2024-03-19 14:16:57 -0400

    Update rockgame.js

--- rockgame.js
+++ rockgame.js
@@ -1,1 +1,18 @@
+var computerValue = "rock"
 
+function checkWinner() {
+  var computerValue = computerShoot();
+}
+
+function computerShoot() {
+  const dict = {1 = }
+  var num = getRandomInt(1,4);
+  
+  return value;
+}
+
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
